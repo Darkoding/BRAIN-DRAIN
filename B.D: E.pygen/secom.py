dfd6e477c007aecb3b614aaaffae4c8597dc9c47
Darkoding <154726644+Darkoding@users.noreply.github.com>
2024-06-03 23:14:09 -0600

    Update secom.py

--- B.D: E.pygen/secom.py
+++ B.D: E.pygen/secom.py
@@ -1,3 +1,4 @@
+#primera funcionn definida. aplicaciones de conocimiento basico de variables.
 def obtener_complemento(dna):
     complemento = {
        'A': 'T',
@@ -5,9 +6,9 @@ def obtener_complemento(dna):
        'C': 'G',
        'G': 'C'
     }
-    return''.join([complemento[base]for base in dna])
+    return''.join([complemento[base]for base in dna]) #encontrar una forma rapida para carrear tantas bases
     secuencia="GGGTTAATGACTAATCAGCCCATGATCACACATAACTGTGGTGTCATGCATTTGGTATTTTTAATTTTTAGGGGGTCGAACTTGCTATGACTCAGCTATGACCTAAAGGTCCTGACTCAGTCAAATATAATGTAGCTGGG"
     print("secuencia original", secuencia)
-    complemento= obtener_complemento(secuencia)
+    complemento= obtener_complemento(secuencia) #supuestamente para debug. Revisar más tarde
     print("secuencia complementaria",complemento)
     
